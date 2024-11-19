1fdd04e7d0a169dfa70170c21f8c8e78801c8092
fishnchipa <wilsoncaox@gmail.com>
2024-11-19 16:41:30 +1100

    added header component

---
+++ components/Header.tsx
@@ -0,0 +1,12 @@
+import Navbar from "@/components/Navbar";
+
+export default function Header() {
+
+
+
+  return (
+    <header className="h-[70px] flex justify-between">
+      <Navbar />
+    </header>
+  )
+}
