f623235c8eedb7b445a5dd68c2a473f3b8a35c47
fishnchipa <wilsoncaox@gmail.com>
2024-11-20 11:42:01 +1100

    landing page head line

---
+++ components/HeadlineBackDrop.tsx
@@ -0,0 +1,30 @@
+
+export default function HeadlineBackDrop() {
+  return (
+    <svg 
+      width="1919" 
+      height="546" 
+      viewBox="0 0 1919 546" 
+      fill="none" 
+      xmlns="http://www.w3.org/2000/svg"
+      className="absolute -z-10"
+    >
+      <g clipPath="url(#clip0_569_2)">
+      <rect width="1920" height="546" transform="translate(-1)" fill="url(#paint0_linear_569_2)"/>
+      <path d="M140.094 -59.6985L264.5 82L-7.60732 324.437L-133.218 182.959L140.094 -59.6985Z" fill="#F0F0F0"/>
+      <rect x="-1" width="1920" height="83" fill="#F0F0F0"/>
+      <rect x="-1" y="463" width="1920" height="83" fill="#F0F0F0"/>
+      <path fillRule="evenodd" clipRule="evenodd" d="M308.404 83C308.567 81.3555 308.65 79.6875 308.65 78C308.65 50.3858 286.265 28 258.65 28C231.036 28 208.65 50.3858 208.65 78C208.65 93.0949 215.339 106.627 225.915 115.795L244.832 99L244.909 99.0868C253.697 89.5709 266.135 83.4764 280 83.0267V83H281.65H308.404Z" fill="#F0F0F0"/>
+      </g>
+      <defs>
+      <linearGradient id="paint0_linear_569_2" x1="0" y1="273" x2="1920" y2="273" gradientUnits="userSpaceOnUse">
+      <stop stopColor="#4A4A4A"/>
+      <stop offset="1" stopColor="#272727"/>
+      </linearGradient>
+      <clipPath id="clip0_569_2">
+      <rect width="1920" height="546" fill="white" transform="translate(-1)"/>
+      </clipPath>
+      </defs>
+    </svg>
+  )
+}
