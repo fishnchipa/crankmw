dfe3ee21e8903cd89f01e71fcd5d58b0e2015515
fishnchipa <wilsoncaox@gmail.com>
2024-11-20 23:49:23 +1100

    wrapper for links

---
+++ components/Anchor.tsx
@@ -0,0 +1,21 @@
+
+import Link from "next/link";
+
+type AnchorProps = {
+  href: string
+  className?: string
+} & React.PropsWithChildren
+
+export default function Anchor({href, className, children}: AnchorProps) {
+
+  return (
+    <Link
+      className={className}
+      href={href}
+    >
+      {children}
+    </Link>
+
+
+  )
+}
