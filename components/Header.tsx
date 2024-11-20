12224c76e869e89f0240c111726af3fb8a274136
fishnchipa <wilsoncaox@gmail.com>
2024-11-20 11:42:21 +1100

    increased z index

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,7 +5,7 @@ export default function Header() {
 
 
   return (
-    <header className="w-full h-[70px] flex justify-center fixed top-0">
+    <header className="w-full h-[70px] flex justify-center fixed top-0 z-10">
       <Navbar />
     </header>
   )
