7d781022a0de5d97a27fa47309f39d28760d8af9
fishnchipa <wilsoncaox@gmail.com>
2024-11-20 23:48:54 +1100

    changed opacity

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,129 +1,133 @@
 "use client"
 
-import Link from "next/link";
 import Selector from "@/components/Selector";
 import ShoppingCart from "@/components/ShoppingCart";
 import Image from "next/image";
 import { useEffect, useState } from "react";
 import { motion } from "framer-motion";
+import Anchor from "./Anchor";
+import Button from "./Button";
+import MenuButton from "./MenuButton";
 
-export default function Navbar() {
+type NavbarProps = {
+  setMenuAction: React.Dispatch<React.SetStateAction<boolean>>
+  menu: boolean
+}
+
+const nav = ["Home", "Products", "Tuning", "Contact", "Search"]
+
+const iconAnimate = {
+  initial: { opacity: 0 },
+  animate: { opacity: 1 },
+  transition: { 
+    duration: 0.3, 
+    delay: 0.5, 
+    ease: "linear" 
+  }
+}
+
+export default function Navbar({ menu, setMenuAction }: NavbarProps) {
   const [selector, setSelector] = useState({ left: 0, width: 0 });
   const [isScrolled, setIsScrolled] = useState(false);
+  const [isWindow, setIsWindow] = useState(false);
+  const [isBurger, setIsBurger] = useState(false);
+
+  useEffect(() => {
+    const activateScroll = () => {
+      setIsScrolled(window.scrollY > window.innerHeight);
+    }
+
+    const activateWindow = () => {
+      setIsWindow(window.innerWidth <= 768);
+      setIsBurger(window.innerWidth <= 550);
+      if (window.innerWidth > 550) {
+        setMenuAction(false);
+      }
+    }
+
+    activateScroll();
+    activateWindow();
+    window.addEventListener("scroll", activateScroll);
+    window.addEventListener("resize", activateWindow);
+
+    return () => {
+      window.removeEventListener("scroll", activateScroll);
+      window.removeEventListener("resize", activateWindow);
+    }
+  }, [setMenuAction])
 
   const handleHover = (event: React.MouseEvent<HTMLLIElement>) => {
     const { offsetLeft, offsetWidth } = event.currentTarget;
     setSelector({ left: offsetLeft, width: offsetWidth });
   };
 
-  useEffect(() => {
-    const activate = () => {
-      if (window.scrollY > window.innerHeight) {
-        setIsScrolled(true);      
-      } else {
-        setIsScrolled(false); 
-      }
-    }
-    activate();
-    window.addEventListener("scroll", activate);
-
-    return () => {
-      window.removeEventListener("scroll", activate);
-    }
-  }, [])
+  const toggleHamburger = () => {
+    setMenuAction(prev => !prev);
+  }
 
   return (
     <nav 
       style={{
-        width: isScrolled ? "100%" : "1000px",
-        clipPath: isScrolled ? "polygon(0% 0, 100% 0, 100% 100%, 0% 100%)" : "polygon(0% 0, 100% 0, 95% 100%, 5% 100%)",
-        background: isScrolled ? "rgb(57, 58, 58)" : "rgba(17, 19, 20, 0.7)",
+        width: isWindow || isScrolled ? "100%" : "1000px",
+        clipPath: isWindow || isScrolled ? "polygon(0% 0, 100% 0, 100% 100%, 0% 100%)" : "polygon(0% 0, 100% 0, 95% 100%, 5% 100%)",
+        background: isWindow || isScrolled ? "rgb(57, 58, 58)" : "rgba(17, 19, 20, 0.7)",
       }}
-      className="flex items-center justify-center font-noto-sans font-normal relative text-white transition-all duration-500"
+      className="transition-all duration-500"
     >
-      {isScrolled && 
-        <motion.div 
-          className="absolute left-5"
-          initial={{
-            opacity: 0
-          }}
-          animate={{
-            opacity: 1
-          }}
-
-          transition={{ 
-            duration: 0.3, 
-            delay: 0.5, 
-            ease: "linear" 
-          }}
-        >
-          <div className="w-[110px] h-[45px] relative">
-            <Image 
-              src="/logo.webp" 
-              alt="logo"
-              className="object-cover"
-              fill
-            />     
-          </div>
-        </motion.div>
-      }
-      <ul className="h-full flex flex-row items-center gap-x-10 relative">
-        <li
-          onMouseEnter={handleHover}
-          className="relative"
-        >
-          <Link href="/">Home</Link>
-        </li>
-        <li
-          onMouseEnter={handleHover}
-          className="relative"
-        >
-          <Link href="/">Products</Link>
-        </li>
-        <li
-          onMouseEnter={handleHover}
-          className="relative"
-        >
-          <Link href="/">Tuning</Link>
-        </li>
-        <li
-          onMouseEnter={handleHover}
-          className="relative"
-        >
-          <Link href="/">Contact</Link>
-        </li>
-        <li
-          onMouseEnter={handleHover}
-          className="relative"
-        >
-          Search
-        </li>
-        <Selector 
-          left={selector.left}
-          width={selector.width}
-        />
-      </ul>
-      {isScrolled && 
-        <motion.div 
-          className="absolute right-5"
-          initial={{
-            opacity: 0
-          }}
-          animate={{
-            opacity: 1
-          }}
-
-          transition={{ 
-            duration: 0.3, 
-            delay: 0.5, 
-            ease: "linear" 
-          }}
-        >
-          <Link href="/">
-            <ShoppingCart />
-          </Link>  
-        </motion.div>
-      }
+    {!isBurger ? <div className="w-full h-full flex items-center justify-between px-5 relative md:justify-center">
+        {(!isWindow && isScrolled) && 
+          <motion.div 
+            className="absolute left-5"
+            {...iconAnimate}
+          >
+            <div className="w-[110px] h-[45px] relative">
+              <Image 
+                src="/logo.webp" 
+                alt="logo"
+                className="object-cover"
+                fill
+              />     
+            </div>
+          </motion.div>
+        }
+        <ul className="h-full flex flex-row items-center gap-x-10 relative font-noto-sans font-normal text-white">
+          {nav.map(item => (
+            <li
+              onMouseEnter={handleHover}
+              className="relative"
+              key={item}
+            >
+              <Anchor href="/">{item}</Anchor>
+            </li>
+          ))}
+          <Selector 
+            left={selector.left}
+            width={selector.width}
+          />
+        </ul>
+        {(isWindow || isScrolled) && 
+          <motion.div 
+            className="absolute right-5"
+            {...iconAnimate}
+          >
+            <Anchor href="/">
+              <ShoppingCart />
+            </Anchor>  
+          </motion.div>
+        }
+      </div> : <div className="w-full h-full flex items-center justify-between px-5">
+        <Button onClick={toggleHamburger}>
+          <MenuButton isOpen={menu}/>
+        </Button>
+        <div className="w-[110px] h-[45px] relative">
+          <Image 
+            src="/logo.webp" 
+            alt="logo"
+            className="object-cover"
+            fill
+          />     
+        </div>
+      </div>}
     </nav>
   )
 }
