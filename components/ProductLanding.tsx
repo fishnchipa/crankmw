ba45e7ef4d36eb2fd2df8e9e17ad72cbb4244c24
fishnchipa <wilsoncaox@gmail.com>
2024-11-20 23:49:15 +1100

    changed color of font

--- components/ProductLanding.tsx
+++ components/ProductLanding.tsx
@@ -1,14 +1,15 @@
-import Link from "next/link";
 import Product from "./Product";
+import Anchor from "./Anchor";
+import Button from "./Button";
 
 export default function ProductLanding() {
 
 
   return (
     <div className="flex flex-col gap-y-3 px-10 lg:px-[200px]">
-      <Link href="/" className="w-fit hover:underline text-moody-gray font-semibold decoration-[3px]" >
+      <Anchor href="/" className="w-fit hover:underline text-moody-gray font-semibold decoration-[3px]" >
         <h1 className="text-[#4A4A4A] text-[25px] sm:text-[30px] xl:text-[34px]">Hot Products</h1>
-      </Link>
+      </Anchor>
       <div className="grid xl:grid-cols-4 md:grid-cols-2 grid-cols-1 w-full sm:gap-x-5 gap-y-10">
         <Product title='5" CMW B58 Catless Downpipe' price={13} sale={{discount: 40}}/>
         <Product title='5" CMW B58 Catless Downpipe' price={13} sale={{discount: 40}}/>
@@ -16,12 +17,12 @@ export default function ProductLanding() {
         <Product title='5" CMW B58 Catless Downpipe' price={13} sale={{discount: 40}}/>
       </div>
       <div className="w-full flex justify-center items-center">
-      <button 
+      <Button 
         className="w-28 h-12 relative group overflow-hidden border-2 border-black hover:text-white mt-10"
       >
-        <span className="absolute inset-0 bg-black transform -translate-x-full group-hover:translate-x-0 transition-transform duration-300 ease-in-out"></span>
-        <span className="relative z-10">VIEW ALL</span>
-      </button>
+        <span className="absolute inset-0 bg-black transform -translate-x-full group-hover:translate-x-0 transition-transform duration-400 ease-in-out"></span>
+        <span className="relative">VIEW ALL</span>
+      </Button>
 
       </div>
     </div>
