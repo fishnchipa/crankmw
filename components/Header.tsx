948c248fc300a2f9e8e4a736f5cba3015ccb2294
fishnchipa <wilsoncaox@gmail.com>
2024-11-20 23:48:33 +1100

    added menu modal

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,12 +1,19 @@
+"use client"
+
 import Navbar from "@/components/Navbar";
+import { useState } from "react";
+import MenuModal from "./MenuModal";
+import { AnimatePresence } from "framer-motion";
 
 export default function Header() {
-
-
+  const [menu, setMenu] = useState(false); 
 
   return (
     <header className="w-full h-[70px] flex justify-center fixed top-0 z-10">
-      <Navbar />
+      <AnimatePresence>
+        {menu && <MenuModal setMenuAction={setMenu}/>}
+      </AnimatePresence>
+      <Navbar setMenuAction={setMenu} menu={menu}/>
     </header>
   )
 }
