9dd2bde6ca68a10e69873d7072678a7746fba2dc
fishnchipa <wilsoncaox@gmail.com>
2024-11-21 21:11:26 +1100

    added mock data

---
+++ lib/mock.ts
@@ -0,0 +1,56 @@
+
+export const brands = [
+  {
+    src: "/11562_300x300.webp",
+    alt: "logo eventuri"
+  },
+  {
+    src:"/apr_logo_2018_rgb_red_300x300.png",
+    alt: "logo apr"
+  },
+  {
+    src: "/BC_FORG_300x300.png",
+    alt: "bc forg logo"
+  },
+  {
+    src: "/Untitled_design_6_300x300.png",
+    alt: "logo design"
+  },
+  {
+    src: "/Untitled_design_6_300x300.png",
+    alt: "logo design"
+  }
+]
+
+export const reviews = [
+  {
+    name: "Stavors C",
+    date: "Dec 21, 2023",
+    img: "/review1.png",
+    alt: "car",
+    desc: "\“ I recently had the pleasure of getting my car tuned with APR Stage 2, and I must say, it has been an absolute game-changer. I wanted to share my experience and express my utmost satisfaction with the results. The transformation this tuning has brought to my car is nothing short of remarkable. \“",
+  },
+  {
+    name: "J.J James",
+    date: "Jan 09, 2023",
+    img: "/tune.jpeg",
+    alt: "tune",
+    desc: "\“ I recently had the pleasure of getting my car tuned with APR Stage 2, and I must say, it has been an absolute game-changer. I wanted to share my experience and express my utmost satisfaction with the results. The transformation this tuning has brought to my car is nothing short of remarkable. \“",
+  },
+  {
+    name: "Donald Trump",
+    date: "Dec 21, 2022",
+    img: "/tune.jpeg",
+    alt: "tune",
+    desc: "\“ I recently had the pleasure of getting my car tuned with APR Stage 2, and I must say, it has been an absolute game-changer. I wanted to share my experience and express my utmost satisfaction with the results. The transformation this tuning has brought to my car is nothing short of remarkable. \“",
+  },
+  {
+    name: "Stavors C",
+    date: "Dec 21, 2023",
+    img: "/review1.png",
+    alt: "car",
+    desc: "\“ I recently had the pleasure of getting my car tuned with APR Stage 2, and I must say, it has been an absolute game-changer. I wanted to share my experience and express my utmost satisfaction with the results. The transformation this tuning has brought to my car is nothing short of remarkable. \“",
+  },
+]
+
+
