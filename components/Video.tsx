0c7420f7debda45819746d0b802ae7e61078ad56
fishnchipa <wilsoncaox@gmail.com>
2024-11-22 14:50:51 +1100

    removed video

--- components/Video.tsx
+++ components/Video.tsx
@@ -9,6 +9,7 @@ export default function Video() {
         <h1 className="text-[34px] font-semibold">EURO TUNING SPECIALIST - A ONE STOP SHOP</h1>
       </div>
       <iframe 
+      
         className="absolute w-full h-full pointer-events-none"
         src={"https://www.youtube.com/embed/l6bZ2UhnWUE?si=sC7rGeky7TZ9Ul36&autoplay=1&controls=0&modestbranding=1&rel=0&mute=1&showinfo=0"} 
         title="720s Flamethrower" 
