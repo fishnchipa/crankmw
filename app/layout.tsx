1f415a46ba497716cb1a192c913d3fcc26083875
fishnchipa <wilsoncaox@gmail.com>
2024-11-26 12:40:07 +1100

    added crimson font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,13 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import { Inter, Noto_Sans } from 'next/font/google'
+import { Inter, Noto_Sans, Crimson_Pro } from 'next/font/google'
 import Footer from "@/components/Footer";
 import Header from "@/components/header/Header";
  
 const inter = Inter({ subsets: ['latin'] })
 const notoSans = Noto_Sans({ subsets: ['latin'], variable: "--noto_sans" });
+const crimson = Crimson_Pro({subsets: ["latin"], variable: "--font-crimson"});
+
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -20,7 +22,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${inter.className} ${notoSans.variable} antialiased`}
+        className={`${inter.className} ${notoSans.variable} ${crimson.variable} antialiased`}
       >
         <Header />
         {children}
