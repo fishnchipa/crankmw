d75d4018063e455d71bad18aa7ed86a4ce36efeb
fishnchipa <wilsoncaox@gmail.com>
2024-11-26 12:39:53 +1100

    added items on product page

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,10 +1,22 @@
+import Filterbar from "@/components/filter/Filterbar";
+import ProductSection from "@/components/product/ProductSection";
 
 export default function Home() {
-
+  
+  
 
   return (
-    <div>
-      Products
+    <div className="flex justify-center mt-[100px] text-[24px]">
+      <div className="px-10 w-full 2xl:w-[1500px] 2xl:p-0">
+        <div className="w-full border-b-[2px] border-[#D1D1D1] font-crimson">
+          <span>{3} Results</span>
+        </div>
+
+        <div className="w-full mt-10 flex flex-row gap-x-10">
+          <Filterbar />
+          <ProductSection />
+        </div>
+      </div>
     </div>
   )
 }
