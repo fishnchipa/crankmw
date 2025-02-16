bd2bbe30757042d140510f02071d3dd5d8f5b61d
fishnchipa <wilsoncaox@gmail.com>
2025-02-16 18:59:04 +1100

    mock

---
+++ lib/mock.ts
@@ -0,0 +1,94 @@
+
+export const brands = [
+  {
+    src: "/11562_300x300.webp",
+    alt: "logo eventuri",
+  },
+  {
+    src: "/apr_logo_2018_rgb_red_300x300.png",
+    alt: "logo apr",
+  },
+  {
+    src: "/BC_FORG_300x300.png",
+    alt: "bc forg logo",
+  },
+  {
+    src: "/Untitled_design_6_300x300.png",
+    alt: "logo design",
+  },
+  {
+    src: "/Untitled_design_6_300x300.png",
+    alt: "logo design",
+  },
+];
+
+export const reviews = [
+  {
+    name: "Stavors C",
+    date: "Dec 21, 2023",
+    img: "/review1.png",
+    alt: "car",
+    desc: "\“ I recently had the pleasure of getting my car tuned with APR Stage 2, and I must say, it has been an absolute game-changer. I wanted to share my experience and express my utmost satisfaction with the results. The transformation this tuning has brought to my car is nothing short of remarkable. \“",
+  },
+  {
+    name: "J.J James",
+    date: "Jan 09, 2023",
+    img: "/tune.jpeg",
+    alt: "tune",
+    desc: "\“ I recently had the pleasure of getting my car tuned with APR Stage 2, and I must say, it has been an absolute game-changer. I wanted to share my experience and express my utmost satisfaction with the results. The transformation this tuning has brought to my car is nothing short of remarkable. \“",
+  },
+  {
+    name: "Donald Trump",
+    date: "Dec 21, 2022",
+    img: "/tune.jpeg",
+    alt: "tune",
+    desc: "\“ I recently had the pleasure of getting my car tuned with APR Stage 2, and I must say, it has been an absolute game-changer. I wanted to share my experience and express my utmost satisfaction with the results. The transformation this tuning has brought to my car is nothing short of remarkable. \“",
+  },
+  {
+    name: "Stavors C",
+    date: "Dec 21, 2023",
+    img: "/review1.png",
+    alt: "car",
+    desc: "\“ I recently had the pleasure of getting my car tuned with APR Stage 2, and I must say, it has been an absolute game-changer. I wanted to share my experience and express my utmost satisfaction with the results. The transformation this tuning has brought to my car is nothing short of remarkable. \“",
+  },
+];
+
+export const productData = {
+  id: "1",
+  title: "5″ CMW B58 CATLESS DOWNPIPE",
+  sku: "2",
+  price: 720.0,
+  discount: 950,
+  preview: [
+    {
+      src: "/eve.webp",
+      alt: "crank",
+    },
+    {
+      src: "/eve.webp",
+      alt: "crank",
+    },
+  ],
+  content: [
+    {
+      type: "text",
+      value:
+        "Increase performance and reduce back-pressure with a high-flow CMW Downpipe. Faster spool, increased power, and a more aggressive exhaust note. Using a cast French head to 5″ downpipe, Handcrafted 304 Stainless Steel construction. ",
+    },
+    {
+      type: "list",
+      value: [
+        "Catless",
+        "Cast French Head",
+        "Gains from 20-25hp with the tune",
+        "Heat Shield",
+        "Fitment guaranteed",
+      ],
+    },
+    {
+      type: "text",
+      value:
+        "Back pressure is significantly reduced by eliminating the restrictive catalytic converter in the factory downpipe with our catless which results in a faster spool, an increase in power & a more aggressive exhaust note. Our downpipe features a full 5″ stainless steel design as well as a CNC 304 grade stainless steel flange that offers a smooth transition from the turbo to the mid pipes. ",
+    },
+  ],
+} 
